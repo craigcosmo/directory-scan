e7e8df204f2a17734c70fcf0fd5248770946b29b
Craig Nguyen <linh.nguyen@aleph-labs.com>
2016-12-30 12:36:17 +0700

    update

--- dist/index.js
+++ dist/index.js
@@ -26,44 +26,54 @@ function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr
  */
 
 function findDup(arr) {
-	var valuesSoFar = Object.create(null);
-	var du = [];
-	for (var i = 0; i < arr.length; ++i) {
-		var value = arr[i];
-		if (value in valuesSoFar) {
-			du.push(value);
+
+	var duplicates = [];
+	var i = void 0,
+	    j = void 0;
+	for (i = 0, j = arr.length; i < j; i++) {
+		if (duplicates.indexOf(arr[i]) === -1 && arr.indexOf(arr[i], i + 1) !== -1) {
+			duplicates.push(arr[i]);
 		}
-		valuesSoFar[value] = true;
 	}
-	return du;
+	return duplicates;
+}
+function pathToFileName(arr) {
+	return arr.map(function (i) {
+		return i.split('/').pop();
+	});
 }
 
 module.exports = {
 	get: function get(dir) {
+		var excluded = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';
+
 		var modules = [];
+		var listF = [];
 		var rootDirScan = _fs2.default.readdirSync(dir).filter(function (i) {
 			return _fs2.default.statSync(dir + i).isDirectory();
 		}).filter(function (i) {
 			return !/^\.|node_modules/.test(i);
 		}) // exclude .git and node_modules
-		.map(function (i) {
-			return i + '/**/';
-		}) // adding the glob's pattern to the directories
-		.map(function (i) {
-			var a = _glob2.default.sync(i);
+		.filter(function (i) {
+			if (excluded) {
+				var regex = new RegExp(excluded);
+				return !regex.test(i);
+			} else {
+				return true;
+			}
+		}).map(function (i) {
+			var a = _glob2.default.sync(i + '/**/');
 			// console.log('a',a)
 			modules.push.apply(modules, _toConsumableArray(a));
+
+			var d = _glob2.default.sync(i + '/**/*.*');
+			listF.push.apply(listF, _toConsumableArray(d));
 		});
 
-		var fileList = (0, _recursiveReaddirSync2.default)('dist');
-		// console.log('a',fileList)
-		var fileNames = fileList.map(function (i) {
-			return i.split("/").pop();
-		});
-		// console.log('f',fileNames)
-		var duplicated = findDup(fileNames);
+		var duplicated = findDup(pathToFileName(listF));
+
 		// console.log('d',duplicated)
-		duplicated.length && console.log('there is duplicate file in your system:', duplicated);
+		duplicated.length && console.log('there are duplicated files in your system: ', duplicated);
 		return modules;
 	}
 };
